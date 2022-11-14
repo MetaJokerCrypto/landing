e6c524749ecb9eb8947ac5745fa71c2ca5929305
Ivan Starikov <vanya.st.ov@gmail.com>
2022-11-14 13:19:04 +0800

    Add if

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,11 +2,13 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    }
-  });
+  if (stage === 'build-html' || stage === 'develop-html') {
+    actions.setWebpackConfig({
+      resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+      }
+    });
+  }
 };
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
