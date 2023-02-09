751711a8c2c06b0fed2997d46a682b4f56daa294
Ivan Starikov <vanya.st.ov@gmail.com>
2023-02-09 20:11:20 +0800

    Add about page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,18 +10,18 @@ export const Header = () => {
           <Logo>MJ</Logo>
         </StyledLink>
         <NavBar>
-          <StyledLink to="/">
-            <NavItem>Play</NavItem>
+        <StyledLink to='/games'>
+            <NavItem>Games</NavItem>
           </StyledLink>
-          <StyledLink to="/">
-            <NavItem>Все проекты</NavItem>
-          </StyledLink>
-          <StyledLink to="/blog">
+          <StyledLink to='/blog'>
             <NavItem>Блог</NavItem>
           </StyledLink>
-          <StyledLink to="">
-            <NavItem>О нас</NavItem>
+          <StyledLink to='/p2b'>
+            <NavItem>P2B</NavItem>
           </StyledLink>
+        <StyledLink to='/about'>
+          <NavItem>О нас</NavItem>
+        </StyledLink>
         </NavBar>
       </HeaderWrapper>
     </Wrapper>
